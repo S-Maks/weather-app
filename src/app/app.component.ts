c4323c8b85911605feaddd6326fc2d6823fc8635
Maks Titok <cool.dan47@mail.ru>
2021-11-29 12:36:05 +0300

    Some fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {NgSelectConfig} from "@ng-select/ng-select";
 export class AppComponent {
   title = 'weather-app';
   constructor(private config: NgSelectConfig) {
-    this.config.notFoundText = 'Местоположение не найдено';
+    this.config.notFoundText = '';
     this.config.appendTo = 'body';
     // set the bindValue to global config when you use the same
     // bindValue in most of the place.
